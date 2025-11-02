b9e366acf8f2ba7a14c881de198418a9dba1e3e7
Mahesh Dheerasinghe <173579843+mjd23e@users.noreply.github.com>
2025-11-02 16:55:41 -0500

    Add setup.py for Li_garnet package
    This setup.py file shows setuptools how to build and install the project as a Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Li_garnet',
+    version='0.1',
+    py_modules=['Li_garnet'],
+    description='Tools and utilities for lithium garnet projects',
+    author='Mahesh Jagathpriya Dheerasinghe',
+    author_email='md2023fsu@gmail.com',
+    url='https://github.com/mjd23e/Li_garnet',
+)
