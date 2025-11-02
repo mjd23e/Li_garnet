d23d2b69043ba39cad8fe04a8e66e54523a9deae
Mahesh Dheerasinghe <173579843+mjd23e@users.noreply.github.com>
2025-11-02 16:58:44 -0500

    Update package name and version in setup.py
    Updated file have more details for the users.

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Li_garnet',
-    version='0.1',
-    py_modules=['Li_garnet'],
+    name='li_garnet',
+    version='0.1.0',
+    packages=find_packages(),
     description='Tools and utilities for lithium garnet projects',
     author='Mahesh Jagathpriya Dheerasinghe',
     author_email='md2023fsu@gmail.com',
     url='https://github.com/mjd23e/Li_garnet',
+    python_requires=">=3.9",
+    install_requires=[
+        "pymatgen>=2024.0.0",
+        "numpy>=1.23",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
 )
